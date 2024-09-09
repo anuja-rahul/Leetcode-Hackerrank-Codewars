92ca2e2895ed5b54ec84ba0da1367225fcd5f76a
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-09-09 14:34:32 +0530

    changes

---
+++ Randoms/2.Sequence-of-a-coin-flip/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field, constr
+
+
+class FlipDataInput(BaseModel):
+    sequence: constr(pattern=r"^[HT]+$", min_length=5, max_length=70) = Field(
+        ..., description="Sequence consisting only of 'H' and 'T' with length between 5 and 70 inclusive."
+    )
+    L: int = Field(..., ge=2, le=8, description="Integer L between 2 and 8 inclusive.")
+    N: int = Field(..., ge=1, le=8, description="Integer N between 1 and 8 inclusive.")
